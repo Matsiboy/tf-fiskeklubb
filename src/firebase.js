ef66ce3d666cd24ba192bab40a8cef5154ddd86c
Matsiboy <mats.ost87@gmail.com>
2026-09-09 12:27:44 +0200

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey:            "DIN_API_KEY",
+  authDomain:        "ditt-prosjekt.firebaseapp.com",
+  projectId:         "ditt-prosjekt-id",
+  storageBucket:     "ditt-prosjekt.appspot.com",
+  messagingSenderId: "123456789",
+  appId:             "1:123456789:web:abcdef123456"
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
