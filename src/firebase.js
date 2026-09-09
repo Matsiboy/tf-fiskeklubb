72ed484d4c983515e52300b061416437f4fecc76
Matsiboy <mats.ost87@gmail.com>
2026-09-09 12:31:26 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey:            "DIN_API_KEY",
-  authDomain:        "ditt-prosjekt.firebaseapp.com",
-  projectId:         "ditt-prosjekt-id",
-  storageBucket:     "ditt-prosjekt.appspot.com",
-  messagingSenderId: "123456789",
-  appId:             "1:123456789:web:abcdef123456"
+  apiKey:            "AIzaSyC6ApDhT_BQObrhbq_l9SQ-EQGsddduidw",
+  authDomain:        "fiskeklubb-588ed.firebaseapp.com",
+  projectId:         "fiskeklubb-588ed",
+  storageBucket:     "fiskeklubb-588ed.firebasestorage.app",
+  messagingSenderId: "416234961607",
+  appId:             "1:416234961607:web:afef0e342c7b5ff53f1ce2"
 }
 
 const app = initializeApp(firebaseConfig)
